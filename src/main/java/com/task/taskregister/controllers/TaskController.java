07574fc6df02c777693def2220bd1f565a68b853
KaykyB <kaykyb629@gmail.com>
2023-05-23 10:29:53 -0300

    Modifing the class TaskController

--- src/main/java/com/task/taskregister/controllers/TaskController.java
+++ src/main/java/com/task/taskregister/controllers/TaskController.java
@@ -9,19 +9,32 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Optional;
+
 @Controller
 public class TaskController {
     @Autowired
     TaskService taskService;
     @GetMapping("/create")
-    public String home(){
-        return "create";
+    public ModelAndView home(){
+        ModelAndView mv = new ModelAndView("create");
+        mv.addObject("task", new Task());
+        return mv;
     }
 
     @PostMapping("/create")
     public String create(Task task){
-        taskService.save(task);
+        if (task.getId() != null){
+            var taskModel = new Task();
 
+            taskModel.setId(task.getId());
+            taskModel.setName(task.getName());
+            taskModel.setDate(task.getDate());
+
+            taskService.save(taskModel);
+        } else {
+            taskService.save(task);
+        }
         return "redirect:/list";
     }
     @GetMapping("/list")
@@ -31,9 +44,18 @@ public class TaskController {
         return mv;
     }
     @GetMapping("/edit/{id}")
-    public void edit(@PathVariable(value = "id") Long id){
+    public ModelAndView edit(@PathVariable(value = "id") Long id){
         ModelAndView mv = new ModelAndView("create");
+        Task taskFind = taskService.findAllList().stream().filter(task -> task.getId().equals(id)).findFirst().get();
 
-        //Task taskFind = taskService.findAllList().stream().filter(task -> task.getId().equals(id).;
+        mv.addObject(taskFind);
+        return mv;
+    }
+
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable(value="id") Long id){
+        var taskDeleted = taskService.findById(id);
+
+        taskService.delete(taskDeleted.get());
     }
 }
